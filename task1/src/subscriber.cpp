8833046727f1926d0428e40ed0b3e581ea77290e
Deshna <deshna2thunga0@gmail.com>
2023-05-20 00:41:50 +0530

    missing semicolons T_T

--- task1/src/subscriber.cpp
+++ task1/src/subscriber.cpp
@@ -11,12 +11,12 @@ void cb(const std_msgs::String::ConstPtr& msg){
 }
 int main(int argc , char ** argv){
     string sub1;
-    cout<<"Enter subscriber name : "
+    cout<<"Enter subscriber name : ";
     getline(cin,sub1);
     ros::init(argc,argv,sub1); //this name has to be different
     ros::NodeHandle nr;
     string datas;
-    cout<<"Enter data stream : "
+    cout<<"Enter data stream : ";
     getline(cin,datas);
     ros::Subscriber sub = nr.subscribe(datas,1000,cb); //cb callback function
     ros::spin(); //listens for interrupts doesnt exit until kill signal ctrl c comes
